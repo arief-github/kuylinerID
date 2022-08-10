83dbdba6b4485036bafd18b5c45621af60398a45
github.arief <78076941+arief-github@users.noreply.github.com>
2022-08-10 08:01:13 +0700

    Create Set Up for URL, DB, and API Config

---
+++ src/scripts/global/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+	KEY: '12345',
+	BASE_URL: 'https://restaurant-api.dicoding.dev/',
+	BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium',
+	DATABASE_NAME: 'resto-list-db',
+	DATABASE_VERSION: 1,
+	OBJECT_STORE_NAME: 'restaurants',
+	WEB_SOCKET_SERVER: 'wss://javascript.info/article/websocket/chat/ws',
+}
+
+export default CONFIG;
